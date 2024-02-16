d2b421253d98be2d6ab0dca5abbe0eb2c0a8ff2a
BlondeleauTimeo <timeoblondeleaudubois@gmail.com>
2024-02-16 13:59:19 +0100

    all lumières background du bas

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const lumiere = document.querySelector('.lumiere');
-    const lignesVerticales = document.querySelectorAll('[class^="ligne_verticale"]');
+document.addEventListener("DOMContentLoaded", function() {1000
+    const lumiere = document.querySelector('.lumiere1');
     
     function deplacerLumiere() {
-        const randomChance = Math.floor(Math.random() * 10) + 1;
+        const randomChance = Math.floor(Math.random() * 20) + 1;
         
         if (randomChance <= 1) {
             lumiere.style.transition = 'top 8s linear';
@@ -12,8 +11,584 @@ document.addEventListener("DOMContentLoaded", function() {
             setTimeout(() => {
                 lumiere.style.transition = 'none';
                 lumiere.style.top = '2420px';
-                setTimeout(deplacerLumiere, 20000);
-            }, 20000);
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere2');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere3');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere4');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere5');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere5');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere6');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere7');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere8');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere9');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere20');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere11');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere12');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere13');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere14');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere15');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere16');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere17');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere18');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere19');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere20');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere21');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere22');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere23');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
+        } else {
+
+            setTimeout(deplacerLumiere, 1000);
+        }
+    }
+    
+    deplacerLumiere();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lumiere = document.querySelector('.lumiere24');
+    
+    function deplacerLumiere() {
+        const randomChance = Math.floor(Math.random() * 20) + 1;
+        
+        if (randomChance <= 1) {
+            lumiere.style.transition = 'top 8s linear';
+            lumiere.style.top = `3000px`;
+
+            setTimeout(() => {
+                lumiere.style.transition = 'none';
+                lumiere.style.top = '2420px';
+                setTimeout(deplacerLumiere, 30000);
+            }, 30000);
         } else {
 
             setTimeout(deplacerLumiere, 1000);
